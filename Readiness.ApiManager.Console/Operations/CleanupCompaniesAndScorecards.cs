43b4e54c08e4372a269d7734848fd3c7f235ce74
Dion Scher <dion.scher@ansarada.com>
2018-06-29 14:17:46 +1000

    Added check and delete of scorecards

--- Readiness.ApiManager.Console/Operations/CleanupCompaniesAndScorecards.cs
+++ Readiness.ApiManager.Console/Operations/CleanupCompaniesAndScorecards.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Ansarada.Struse.Gateway.Core.Paging;
+using Ansarada.Struse.Gateway.Dto;
 using Ansarada.Struse.Gateway.Dto.Company;
+using Ansarada.Struse.Gateway.Dto.Scorecard;
 using Readiness.ApiManager.Console.Request;
 using Readiness.ApiManager.Console.Response;
 
@@ -26,20 +29,60 @@ namespace Readiness.ApiManager.Console.Operations
 			};
 			Client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("AdHoc", "0.0.0.0"));
 
+			System.Console.WriteLine("Authorising...");
 			await SetBearerToken();
 
-			//var response1 = await Client.GetAsync("v1/scorecards?isTemplate=false&includeScore=false&limit=1000");
-			//response1.EnsureSuccessStatusCode();
-			//var scorecards = await response1.Content.ReadAsAsync<PagedResult<ScorecardDto>>();
-
-			var response2 = await Client.GetAsync("v1/companies?id=455");
+			System.Console.WriteLine("Getting companies");
+			var response2 = await Client.GetAsync("v1/companies");
 			response2.EnsureSuccessStatusCode();
 			var companies = await response2.Content.ReadAsAsync<IEnumerable<CompanyDto>>();
+			
+			var listOfCompanies = companies.Where(x => x.Name.StartsWith("API Tests - Company"));
 
-			var deleteResponse = await Client.DeleteAsync($"v1/companies/{companies.First().Id}");
+			foreach (var company in listOfCompanies)
+			{
+				System.Console.WriteLine($"Company {company.Name} = {company.Id}");
+
+				var scs = await GetScorecards(company.Id);
+
+				foreach (var sc in scs.Data)
+				{
+					var del = await DeleteScorecard(sc.Id);
+				}
+
+				var deleteResponse = await DeleteCompany(company.Id);
+			}
+		}
+
+		private async Task<PagedResult<ScorecardDto>> GetScorecards(int companyId)
+		{
+			var response = await Client.GetAsync($"v1/scorecards?companyId={companyId}&includeScore=false");
+			response.EnsureSuccessStatusCode();
+			return await response.Content.ReadAsAsync<PagedResult<ScorecardDto>>();
+		}
+
+		private async Task<DeletedDto> DeleteCompany(int companyId)
+		{
+			HttpResponseMessage deleteResponse;
+
+			try
+			{
+				deleteResponse = await Client.DeleteAsync($"v1/companies/{companyId}");
+				deleteResponse.EnsureSuccessStatusCode();
+				return await deleteResponse.Content.ReadAsAsync<DeletedDto>();
+			}
+			catch (Exception e)
+			{
+				System.Console.WriteLine(e);
+				throw;
+			}
+		}
+
+		private async Task<DeletedDto> DeleteScorecard(int scorecardId)
+		{
+			var deleteResponse = await Client.DeleteAsync($"v1/scorecards/{scorecardId}");
 			deleteResponse.EnsureSuccessStatusCode();
-
-			//var c = companies.Where(x => x.Name.StartsWith("API Tests - Company"));
+			return await deleteResponse.Content.ReadAsAsync<DeletedDto>();
 		}
 
 		protected async Task SetBearerToken()
